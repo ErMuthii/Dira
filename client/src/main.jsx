724d55760a4aa98f9994b0536d18a2b91f44d25b
ErMuthii <er.muthii@gmail.com>
2025-03-22 17:15:39 +0300

    Updated packages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { NextUIProvider } from "@nextui-org/react";
+import { HeroUIProvider } from "@heroui/react";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <NextUIProvider>
+      <HeroUIProvider>
         <App />
-      </NextUIProvider>
+      </HeroUIProvider>
     </BrowserRouter>
   </StrictMode>
 );
